ff610135013aad90ce9601dbffbbd440aca37955
Nixon <lazaronixon@hotmail.com>
2024-02-16 20:05:43 -0300

    sqlite3

--- db.rb
+++ db.rb
@@ -2,15 +2,18 @@ require "logger"
 LOGGER = Logger.new($stdout)
 LOGGER.level = ENV.fetch("RODA_LOG_LEVEL", "info")
 
-database_url = ENV.fetch("DATABASE_URL") { "extralite://storage/development.sqlite" }
+database_url = ENV.fetch("DATABASE_URL") { "sqlite://storage/development.sqlite" }
 
 after_connect = proc do |db|
-  db.busy_timeout = 5
-  db.pragma(journal_mode: :wal)
-  db.pragma(synchronous: :normal)
-  db.pragma(journal_size_limit: 67108864)
-  db.pragma(mmap_size: 134217728)
-  db.pragma(cache_size: 2000)
+  db.execute "PRAGMA journal_mode=wal;"
+  db.execute "PRAGMA synchronous=normal;"
+  db.execute "PRAGMA mmap_size=134217728;"
+  db.execute "PRAGMA journal_size_limit=67108864;"
+  db.execute "PRAGMA cache_size=2000;"
+
+  db.busy_handler do |count|
+    (count <= 100).tap { |result| sleep count * 0.001 if result }
+  end
 end
 
 require "sequel/core"
