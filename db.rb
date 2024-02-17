ee8b53da1050aaa1a5e364ba1fb9ea3c71b1bb3e
Nixon <lazaronixon@hotmail.com>
2024-02-17 00:24:05 -0300

    extralite more config

--- db.rb
+++ db.rb
@@ -1,13 +1,17 @@
-# require "extralite"
-# Extralite.on_progress do |busy|
-#   sleep 1 * 0.001 if busy
-# end
-#
-
 require "logger"
 LOGGER = Logger.new($stdout)
 LOGGER.level = ENV.fetch("RODA_LOG_LEVEL", "info")
 
+database_url = ENV.fetch("DATABASE_URL") { "extralite://storage/development.sqlite" }
+
+after_connect = proc do |db|
+  db.pragma(journal_mode: :wal)
+  db.pragma(synchronous: :normal)
+  db.pragma(journal_size_limit: 67108864)
+  db.pragma(mmap_size: 134217728)
+  db.pragma(cache_size: 2000)
+  db.busy_timeout = 9999
+end
+
 require "sequel/core"
-DB = Sequel.connect(ENV.fetch("DATABASE_URL") { "extralite://storage/development.sqlite" })
-DB.loggers << LOGGER
+DB = Sequel.connect(database_url, logger: LOGGER, after_connect: after_connect)
