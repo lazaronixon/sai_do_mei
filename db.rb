9801ea432cf767738d3186eb9c0919e8274bdeb0
Nixon <lazaronixon@hotmail.com>
2024-02-17 00:31:40 -0300

    Update db.rb

--- db.rb
+++ db.rb
@@ -5,13 +5,14 @@ LOGGER.level = ENV.fetch("RODA_LOG_LEVEL", "info")
 database_url = ENV.fetch("DATABASE_URL") { "extralite://storage/development.sqlite" }
 
 after_connect = proc do |db|
+  db.busy_timeout = 5
   db.pragma(journal_mode: :wal)
   db.pragma(synchronous: :normal)
   db.pragma(journal_size_limit: 67108864)
   db.pragma(mmap_size: 134217728)
   db.pragma(cache_size: 2000)
-  db.busy_timeout = 9999
 end
 
 require "sequel/core"
 DB = Sequel.connect(database_url, logger: LOGGER, after_connect: after_connect)
+DB.transaction_mode = :immediate
